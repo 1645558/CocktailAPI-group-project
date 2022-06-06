9ebfdb3ba19891167aa28c6845f572560ea90c70
Rare Raptor <connorwtuncc@gmail.com>
2022-06-06 13:12:56 -0400

    added function to display different pages in script, added buttons, text input etc. in html

--- script.js
+++ script.js
@@ -2,11 +2,131 @@
 // www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
 // www.thecocktaildb.com/api/json/v1/1/random.php
 
-var ownAPI = "1"
-var randBtn = document.querySelector("#btnRandom")
+var randBtn = document.querySelector("#btnRandomScreen");
+var cocktailBtn = document.querySelector('#btnSearchScreen');
+var ingredientBtn = document.querySelector('#btnSearchIngredients');
+var mainEl = document.querySelector('.main');
+var searchCocktailEl = document.querySelector('.search-cocktail');
+var randomCocktailEl = document.querySelector('.random-cocktail');
+var searchIngredientEl = document.querySelector('.search-ingredient');
+var state = 'main';
+
+//search by ingredient name
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka")
+.then(function(response) {
+    console.log(response)
+    return response.json()
+})
+.then(function(data) {
+    console.log(data)
+})
+.catch(function(err) {
+    console.log(err)
+});
+
+//search by cocktail name
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+.then(function(response) {
+    console.log(response)
+    return response.json()
+})
+.then(function(data) {
+    console.log(data)
+})
+.catch(function(err) {
+    console.log(err)
+});
+
+//search random cocktail
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+.then(function(response) {
+    console.log(response)
+    return response.json()
+})
+.then(function(data) {
+    console.log(data)
+})
+.catch(function(err) {
+    console.log(err)
+});
+
+//search random meal
+fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+.then(function(response) {
+    console.log(response)
+    return response.json()
+})
+.then(function(data) {
+    console.log(data)
+})
+.catch(function(err) {
+    console.log(err)
+});
 
 var getUserDrink
-var nameURL = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-fetch(nameURL)
+
+function init() {
+    displayState()
+};
+
+function displayState() {
+    if (state === 'main') {
+      mainEl.style.display = 'block';
+      searchCocktailEl.style.display = 'none';
+      randomCocktailEl.style.display = 'none';
+      searchIngredientEl.style.display = 'none';
+
+    }
+    if (state === 'search-cocktail') {
+        mainEl.style.display = 'none';
+        searchCocktailEl.style.display = 'block';
+        randomCocktailEl.style.display = 'none';
+        searchIngredientEl.style.display = 'none';
+    }
+    if (state === 'random-cocktail') {
+        mainEl.style.display = 'none';
+        searchCocktailEl.style.display = 'none';
+        randomCocktailEl.style.display = 'block';
+        searchIngredientEl.style.display = 'none';
+    }
+    if (state === 'search-ingredient') {
+        mainEl.style.display = 'none';
+        searchCocktailEl.style.display = 'none';
+        randomCocktailEl.style.display = 'none';
+        searchIngredientEl.style.display = 'block';
+    }
+};
+
+randBtn.addEventListener('click', function() {
+    state = 'random-cocktail'
+    displayState();
+});
+
+cocktailBtn.addEventListener('click', function() {
+    state = 'search-cocktail'
+    displayState();
+});
+
+ingredientBtn.addEventListener('click', function() {
+    state = 'search-ingredient'
+    displayState();
+});
+
+init();
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
 
 
